705d987d52a4969f1103289df9754ef6f3b985f0
Michael Nyika <michael.nyika@unity3d.com>
2023-04-14 15:59:02 +0200

    so far so good

--- src/UpstarFun.hs
+++ src/UpstarFun.hs
@@ -7,18 +7,36 @@ import Control.Lens.Combinators (Profunctor, dimap)
 class Profunctor p where
     dimap :: (a' -> a) -> (b -> b')-> p a b -> p a' b'
 
+class Functor f where
+    fmap :: (a -> b) -> f a -> f b
 
 --}
 
--- Our datatype 
-data Upstar f a b = Upstar { unUpstar :: a -> f b }
+-- Let us now invent our own Upstar
+data OpticalUpstar f' a' b' = OpticalUpstar {unstar :: a' -> f' b'}
 
 
 --Make it a Profunctor
 -- Whatever is outputted from u is a functor of something, and we already have our fmap function g, we have to fmap!
-instance Functor f => Profunctor (Upstar f) where
-    dimap h g (Upstar u)     =  (Upstar (fmap g . u . h) ) 
+instance Functor f' => Profunctor (OpticalUpstar f') where
+    dimap h g (OpticalUpstar u)     =  (OpticalUpstar (fmap g . u . h) ) 
 
+---------------------------------------------------------------------------------
 
--- Let us now invent our own Upstar
-data OpticalUpstar f' a' b' = OpticalUpstar {unstar :: a' -> f' b'}
+-- Lets invent more types to use in our example
+
+-- this will help be our functor
+newtype OpFunc a            = OpFunc a 
+
+-- we will be going from this type to a (To) type
+newtype From a              = From a
+
+-- we are going to this type 
+newtype To a                = To a 
+
+---------------------------------------------------------------------------------
+
+--First let's make our OpFunc a functor, since it will be represented in f'
+
+instance Functor OpFunc where
+    fmap f (OpFunc x)        = OpFunc (f x)
