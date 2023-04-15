73b2d2eb6ad32b696af1526d8daaae3e87d83f89
Michael Nyika <michael.nyika@unity3d.com>
2023-04-15 12:18:26 +0200

    ok

--- src/DownstarFun.hs
+++ src/DownstarFun.hs
@@ -14,9 +14,13 @@ class Profunctor p where
 
 class Functor w => Comonad w where
     extract :: w a -> a
-    duplicate :: w a -> w (w a)
-    extend :: (w a -> b) -> w a -> w b
+
 
 --}
 
 -- Imagining a type that encompasses the idea of a Downstar
+newtype OpticalDownstar f a b = OpDownstar { downer :: f a -> b }
+
+-- Ok, lets go ahead and make it a Profunctor
+instance Functor f => Profunctor (OpticalDownstar f) where
+  dimap h g ( OpDownstar d ) = OpDownstar (  g . d . fmap h )
