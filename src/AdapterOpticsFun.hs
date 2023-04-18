406f8e059fd0f7ea9a4a10de900e9f2aa2773909
Michael Nyika <michael.nyika@unity3d.com>
2023-04-18 13:38:00 +0200

    that's ok

--- src/AdapterOpticsFun.hs
+++ src/AdapterOpticsFun.hs
@@ -58,7 +58,7 @@ preAdapt :: s' -> Raw Old
 preAdapt  = undefined
 
 -- We also need a covariant function that takes our profunctor output (t) and potentially manipulates it further 
-postAdapt ::Ripe New -> t'
+postAdapt :: Ripe New -> t'
 postAdapt = undefined
 
 -- Now we invent a function that represents how our adapter works primarily
@@ -71,3 +71,42 @@ unAdapt  = undefined
 
 
 ---------------------------------------------------------------------------------
+
+-- Let's define our transformation with what we have so far: 
+--This Profunctor goes from types : s' -> t' (choosing (Raw Old) and (Ripe New) as s' and t')
+--adapterP :: FunAdapter Old New s' t'
+adapterP :: FunAdapter Old New (Raw Old) (Ripe New)
+adapterP = dimap preAdapt postAdapt (FAdapter adapt unAdapt)
+
+-- We can write what we have above kinda like this:
+--adapterP  :: FunAdapter a b s' t'        <<------- (which can be represented like this: p a b)
+
+-- That's fine, but what if we wanted this:
+--adapterP' :: FunAdapter s t s t         <<------- (which can be represented like this: p s t) 
+
+-- In order words, we need a function that will have this type: 
+-- myUnknownFunction :: p a b -> p s t
+
+-- Remember waaaaaaaay at the top of the page, remember we wrote this wierd thing:         ---->>>>     Optic p a b s t = p a b -> p s t
+-- Looks kinda like what we want to achieve in the "myUnknownFunction", right? 
+
+-- Let's give that function a name, something Adapter-rish:
+--adapterOptical :: Profunctor p => p a b -> p s t 
+
+
+-- What would our final Signature looks like, if we filled in the abstract types (a, b, s, t) with types we already invented?
+ --adapterOptical :: FunAdapter Old New s' t' -> FunAdapter (Raw Old) (Ripe New) (Raw Old) (Ripe New)
+ -- Now since s' can be supposed to be (Raw Old) and t' can be supposed to be (Ripe New) for all intents and purposes, we can simply the signature
+
+ -- So lets invent it now: 
+adapterOptical :: FunAdapter Old New (Raw Old) (Ripe New) -> FunAdapter Old New (Raw Old) (Ripe New)
+adapterOptical (FAdapter inside outside) = dimap inside outside (FAdapter id id)
+
+-- Explanation : outside function is producing type a, and our "to" adapter function  needs to produce the same type as well: so why not use the identity function
+-- Also        : whatever the "fro" adapter function produces is of type t, but know that we'll just need to hand that off to "outside" so who not use the identity function to take a "t" and produce a "t", handing it off, so our "b" might as well be a "t" in the fro-definition
+
+--But how can we actually take advantage of the adapterP profunction we made earlier? Just hand it over to our Optical
+useOptical :: FunAdapter Old New (Raw Old) (Ripe New)
+useOptical = adapterOptical adapterP
+
+      
