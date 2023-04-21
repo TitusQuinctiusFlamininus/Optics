58815c7c4138462499b20ef20eb562549aec49ec
Michael Nyika <michael.nyika@unity3d.com>
2023-04-21 10:38:59 +0200

    initial definitions

---
+++ src/LensOpticsFun.hs
@@ -0,0 +1,25 @@
+module LensOpticsFun where
+
+
+import Control.Lens.Combinators (Profunctor, dimap)
+
+{--
+
+To understand this better, read from top to bottom, in the style 
+that the code was slowly built up
+
+class Profunctor p where
+  lmap  :: (c -> a) -> p a b -> p c b
+  rmap  :: (b -> d) -> p a b -> p a d
+  dimap :: (c -> a) -> (b -> d) -> p a b -> p c d
+
+where p is a Profunctor : 
+type Optic p a b s t = p a b -> p s t
+
+--}
+
+-- This time we are dealing with the powerful concept of a lens
+-- Let's roll our own 
+data OpticalLens a b s t = OptLens { look :: s      -> a, 
+                                     edit :: (b, s) -> t 
+                                   }
