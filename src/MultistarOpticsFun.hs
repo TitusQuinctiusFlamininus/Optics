e553a9494fb26d881868de9253c6c66e54ba2a47
Michael Nyika <michael.nyika@unity3d.com>
2023-04-25 11:47:56 +0200

    ok

--- src/MultistarOpticsFun.hs
+++ src/MultistarOpticsFun.hs
@@ -38,14 +38,14 @@ instance Functor f => Profunctor (Multistar f a b) where
     dimap h g (Multistar u d)     =    Multistar (u . id) (g . d . fmap h)
 
 -- Explanation: 
-    -- (For Multistar's left-hand-side function ): u takes something of type a ; we know h must produce that type; so h is really: (some_type_call_it_a' -> a): Why not just assume a' is identical to a and use identity. Easy enough
+    -- (For Multistar's left-hand-side function ): u takes something of type a ; let's assume h can produce that type; if so, h is really: (some_type_call_it_a' -> a): Why not just assume a' is identical to type a, then use identity? Easy enough
     -- (For Multistar's right-hand-side function): 
     --            --  Notice first  : The left hand side (as explained above), goes from : (a -> f b) . The type a here is not necessarily identical to a' in h; it could be a', but it could also be something else
     --                       second : s and t are types that 'make up' the concept of a multistar profunctor here but they dont appear as types on the left, only on the right; 
     --                       third  : our profunctor still needs to go from : (input_of_h -> output_of_g) : the right-hand-side will need to fully resolve this mapping somehow 
                   --  We need to go from some (input_of_h -> something) : so let's start with h : that takes care of this problem 
                   --  (d . h) makes no sense because d takes a functor of some type: So fmap is necessary : so now we have (d . fmap (a' -> ?)) : The ? turns out to be s : Why? Because u takes (f s) : So h is really : (a' -> s)
-                  --  (d . fmap h) produces a type t: What consumes things of type t? loooks like g . So we hand it the result, and we have :  (g . d . fmap h)        
+                  --  (d . fmap h) produces a type t: What consumes things of type t? loooks like g . So let's compose to get :  (g . d . fmap h)        
                   --  (g . d . fmap h)  seems to be of type : (input_of_h -> output_of_g), solving the type-mapping problem
 
 ---------------------------------------------------------------------------------
