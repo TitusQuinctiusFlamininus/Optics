a1f53186be94df948a1d7097f05e46c486f1ca47
Michael Nyika <michael.nyika@unity3d.com>
2023-05-09 09:24:46 +0200

    easier on the eyes

--- src/UpstarCoCartesianFun.hs
+++ src/UpstarCoCartesianFun.hs
@@ -37,5 +37,5 @@ instance Functor f =>  Profunctor (CoCartesian f) where
 
 -- Going ahead and Co-Strengthening it
 instance (Applicative f) =>  Choice (CoCartesian f)     where
-  left'  (ChoiceUpStar  u)              =    ChoiceUpStar $ either (\a -> Left <$> (u a)) (\c -> Right <$> (pure c))                                                             
+  left'  (ChoiceUpStar  u)              =    ChoiceUpStar . either ((Left <$>) . u) $ ((Right <$>) . pure)                                                           
   right' (ChoiceUpStar  u)              =    ChoiceUpStar (\(Right a)  -> Right    <$> (u a))
