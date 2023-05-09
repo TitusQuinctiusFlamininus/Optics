5773c2232f8ec088e7164bd820d9f1fffedeecd4
Michael Nyika <michael.nyika@unity3d.com>
2023-05-09 09:57:40 +0200

    good

--- src/UpstarCoCartesianFun.hs
+++ src/UpstarCoCartesianFun.hs
@@ -36,6 +36,18 @@ instance Functor f =>  Profunctor (CoCartesian f) where
 
 
 -- Going ahead and Co-Strengthening it
+-- Note that f needs to be at least an Applicative, not just a Functor
 instance (Applicative f) =>  Choice (CoCartesian f)     where
   left'  (ChoiceUpStar  u)              =    ChoiceUpStar . either ((Left <$>) . u   ) $ ((Right <$>) . pure)                                                           
   right' (ChoiceUpStar  u)              =    ChoiceUpStar . either ((Left <$>) . pure) $ ((Right <$>) . u   )               
+
+  -- Let's explain how we got to this arrangement: 
+       -- In Upper, when we co-strengthen, instead of (a  -> f b), we are now going like this: ((Either a c)  -> f (Either b c))
+       -- Ok, so the input could be Left or Right; So in a CASE statement, we could have something like: 
+       --                ----->>>   If it's Left, then we have access to the type a , give it to u, and we get (f b); fmap it with a "Left" and we get: 
+       --                                        Left  a     ->  fmap Left (u a) 
+       --                ----->>>   If it's Right, then we have access to the type c, lift it into the applicative; fmap it with a "Right" and we get: 
+       --                                        Right c     ->  fmap Right (pure u) 
+       -- But from our FP basics, we know this function:          either :: (a -> c) -> (b -> c) -> Either a b -> c
+       -- The two parts of our case statement represent the first two function of either, and we already have the Either itself, so we simply shorten it to the above.
+
