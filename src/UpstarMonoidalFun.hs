178f75bc4a106c922a170e504e75d1bab2219473
Michael Nyika <michael.nyika@unity3d.com>
2023-05-10 13:03:51 +0200

    looks ok

--- src/UpstarMonoidalFun.hs
+++ src/UpstarMonoidalFun.hs
@@ -13,15 +13,31 @@ class Profunctor p where
   rmap  :: (b -> d) -> p a b -> p a d
   dimap :: (c -> a) -> (b -> d) -> p a b -> p c d
 
+class Profunctor p => Monoidal p where
+  par   :: p a b  -> p c d -> p (a, c) (b, d)
+  empty :: p () ()
+
+
 class Functor f where
     fmap :: (a -> b) -> f a -> f b
 
 --}
 
+class Profunctor p => Monoidal p where
+  par   :: p a b  -> p c d -> p (a, c) (b, d)
+  empty :: p () ()
+
 -- Ok, Let us invent our own Upstar
-newtype MonoStar f  a  b      =   MonoidalStar { unstar ::  a  ->  f b }
+newtype MonoStar f  a  b                     =   MonoidalStar { unstar ::  a  ->  f b }
 
 
--- Making it a Profunctor 
+
+-- Alright, making it a Profunctor...
 instance Functor f =>  Profunctor (MonoStar f) where
-    dimap h g (MonoidalStar r)          =    MonoidalStar (fmap g . r . h) 
+    dimap h g (MonoidalStar r)               =   MonoidalStar (fmap g . r . h) 
+
+
+-- And now making it Monoidal...
+instance Applicative f =>  Monoidal   (MonoStar f) where
+    par   (MonoidalStar v) (MonoidalStar w)  =   MonoidalStar (\x  ->  ((,) <$> (v . fst $ x)) <*>  (w . snd $ x))
+    empty                                    =   MonoidalStar pure 
