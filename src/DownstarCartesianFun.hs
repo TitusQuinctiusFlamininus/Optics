fd78b8885b2ce343ae6430c9995443f49e73dfa6
Michael Nyika <michael.nyika@unity3d.com>
2023-05-12 14:32:38 +0200

    looks ok now

--- src/DownstarCartesianFun.hs
+++ src/DownstarCartesianFun.hs
@@ -3,6 +3,7 @@ module DownstarCartesianFun where
 
 import Control.Lens.Combinators    (Profunctor, dimap)
 
+
  {--
 
 To understand this better, read from top to bottom, in the style 
@@ -34,23 +35,39 @@ instance Functor f => Profunctor (CartesianDown f) where
   dimap h g ( StrongDown d )         =   StrongDown (  g . d . fmap h )
 
 
+
 -- Ok, so far so good. Now let's make it Strong
+
 {--
+
 
 Why is it not possible to establish a Strong instance for Downstar?
 ----------->>>>>      First  : Instead of going like this:  (f a  ->  b) , we are going like this:  (f (a, c)   ->   (b, c))
---                    Second : Ok, so we have : f (a, c) as input; Let's fmap with a function that takes (a, c)
---                    Third  : If we take (a, c) and simply map to a, then the entire fmap computation would produce ; (f a); Ok so far.....
---                    Fourth : What can we do with (f a) ? We can apply d to this, giving us : b
---                    So it looks like this now                      :   (d . fmap fst)
---                    Fifth  : We don't need just b, but : (b, c) ; But there is no way to bring a new type c, into scope! 
---                    We need the whole structure to look like this  :  ((\m -> d . fmap fst $ m), c)
---         Hence the typechecker complains : "Couldn't match expected type ‘(b, c)’ with actual type ‘b’". Game over.
---         The same applies to the second' cartesian function :(
+                      Second : Ok, so we have : f (a, c) as input; Let's fmap with a function that takes (a, c)
+                      Third  : If we take (a, c) and simply map to a, then the entire fmap computation would produce ; (f a); Ok so far.....
+                      Fourth : What can we do with (f a) ? We can apply d to this, giving us : b
+                      So it looks like this now                      :   (\m -> ((d . fmap fst $ m)))
+                      Fifth  : We don't need just b, but : (b, c) ; So, if we stretch the same logic to the other part of the tuple, we may get what we need
+                      Sixth  : (\m -> ((d . fmap fst $ m),  (fmap snd $ m))) has this type :   (b, f c)  .  
+           Now the typechecker complains : "Couldn't match expected type ‘c’ with actual type ‘f c’". 
+
+          Here is the best we can do :  ---->>>>>>          StrongDown (\m -> ((d . fmap fst $ m),  (fmap snd $ m)))   :: (b, f c)    <<<<<-------
+
+--*******************
+-- DISCLAIMER HERE:
+********************* 
+
+   If the definition were changed from : instance Functor f => Strong (CartesianDown f) where 
+                                  to   : instance Comonad f => Strong (CartesianDown f) where
+   It may be possible to disassociate the second tuple type from its context and continue....    
 
 
+But, as it stands for now: 
 instance Functor f => Strong (CartesianDown f) where
     first'   ( StrongDown d )        =    Not Possible 
     second'  ( StrongDown d )        =    Not Possible 
 
+
  --} 
+
+---------------------------------------------------------------------------------
