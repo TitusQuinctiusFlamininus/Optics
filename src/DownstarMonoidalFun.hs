e257a83979dacfa89411c627c58a9bed2a5651d3
Michael Nyika <michael.nyika@unity3d.com>
2023-05-15 13:09:40 +0200

    Alright

--- src/DownstarMonoidalFun.hs
+++ src/DownstarMonoidalFun.hs
@@ -36,7 +36,7 @@ class functor f => Applicative f where
 
 -- Refreshing our memory.....
 class Profunctor  p => Monoidal p where
-  par        ::   p a b     ->    p c d     -> p (a, c) (b, d)
+  par        ::   p a b     ->    p c d     ->     p (a, c) (b, d)
   empty      ::   p () ()
 
 
@@ -48,4 +48,27 @@ newtype DownMonoid  f  a  b                    =     LowMonoidal   {  down ::   
 
 -- And roll up a Monoidal Profunctor for the Downstar ...
 instance Functor f =>  Profunctor (DownMonoid f) where
-    dimap   h   g    (LowMonoidal u)          =      LowMonoidal  (  g . u . fmap h )
+    dimap   h   g    (LowMonoidal u)           =      LowMonoidal  (  g . u . fmap h )
+
+
+-- Seems we must construct   :   down' ::  ( f (a, c)  -> (b, d) ) for the LHS equation construct :       par  (LowMonoidal u)  (LowMonoidal v) 
+-- This instance seems impossible to construct : Why? 
+-- Explanation : 
+------ >>>>>>  For our par implementation :   
+--             But soon we find out that on the tuple left :   (fmap (u . pure . fst) x)   :: f b     (but we need b, not (f b)
+--             And we also find that on the tuple right    :   (fmap (v . pure . snd) x)   :: f d     (but we need d, not (f d)
+--             Solution ?
+--             If we can disassociate the types from their contexts, we'd be in business; that could be done be raising the abstraction of the f;
+--             We will not tamper with the definition, but leave it as it is. 
+
+------ >>>>>>  For our emtpy implementation :  
+--             What about for empty? : Same problem of context disassociation
+--
+--             instance Functor f  => Monoidal (DownMonoid f) where
+--                     par      (LowMonoidal u)  (LowMonoidal v)  =   Not possible (UNLESS you raise the abstraction of f)
+--                     empty                                      =   Not possible (UNLESS you raise the abstraction of f)
+
+
+
+---------------------------------------------------------------------------------
+
