cd8b3982c8e487028b6294f9e14b092bdf16283c
Michael Nyika <michael.nyika@unity3d.com>
2023-05-23 15:39:47 +0200

    yes

--- src/TaggedCartesianOpticsFun.hs
+++ src/TaggedCartesianOpticsFun.hs
@@ -49,7 +49,7 @@ instance Profunctor StrongTag where
 --                    The tuple needed is simply : (b, c), where : b is really v (see below)
 --                    So far then, we have       : (v, c)  ......
 --                    But what is type c ?       : We have no way of knowing what this type is, how to obtain it, or even how to produce it from other types (or functions). 
---                                                 Our hands are tied.  
+--                                                
 
 -- instance Strong StrongTag where
 --      first'   (CartTag v)               =    ?? (seems impossible)
