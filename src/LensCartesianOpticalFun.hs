4af9c40b3830d9c830b30fc04c57f52a89fcd071
Michael Nyika <michael.nyika@unity3d.com>
2023-05-30 11:42:32 +0200

    looks ok

--- src/LensCartesianOpticalFun.hs
+++ src/LensCartesianOpticalFun.hs
@@ -52,7 +52,14 @@ instance Profunctor (StrongLens a b) where
     dimap  h  g   (SLens v  w)        =    SLens   (v . h  ) (\x  ->  g $ w (fst x, (h $ snd x)))
 
 
-
+-- We can make our Lens Cartesian now. 
+--     Explanation :: -------->>>> For first' : binding the see function with the first tuple type would give us our required composite type (m . fst)
+--                                            : (1) : the first tuple type represents the new types from which we can create new composites (b)
+--                                            : (2) : we search within the second tuple entry for our reference type from which we can build new composites (fst . snd $ x)
+--                                            : take (1) and (2) above as a tuple and : 
+--                                                 --->> we supply the update function with this tuple to obtain our new composite types   (n . ((,) (fst x)) $ (fst . snd $ x))
+--                                                 --->> make sure to associate that new composite type with the passthrough type (c) from the function input, in a tuple : (new_composite, (snd . snd $ x ))
+--                    -------->>>> For second' : Flip the tuple order we obtained in first' ; also, the original composite type (s) is in the second position, which the passthrough type (c) is the in second position                      
 instance Strong (StrongLens a b) where
     first'       (SLens  m   n)       =    SLens   (m . fst) (\x ->   (,)  (n   . ((,) (fst x)) $ (fst . snd $ x)) (snd . snd $ x ))
     second'      (SLens  m   n)       =    SLens   (m . snd) (\x ->   (,)  (fst . snd $ x) (n . ((,) (fst x)) $ (snd . snd $ x   )))
