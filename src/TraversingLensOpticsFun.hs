f8470f3287f95690f18297a24f217e54e8aa5c1a
Michael Nyika <michael.nyika@unity3d.com>
2023-06-02 11:42:59 +0200

    ok

--- src/TraversingLensOpticsFun.hs
+++ src/TraversingLensOpticsFun.hs
@@ -14,27 +14,32 @@ that the code was slowly built up
 
 
 class Profunctor p where
-  lmap  :: (c -> a) -> p a b -> p c b                 <<-----
-                                                             |-------- Either implement these two ...
-  rmap  :: (b -> d) -> p a b -> p a d                 <<-----  
+  lmap  :: (c -> a) -> p a b -> p c b                                                         <<-----
+                                                                                                     |-------- Either implement these two ...
+  rmap  :: (b -> d) -> p a b -> p a d                                                         <<-----  
 
-  dimap :: (c -> a) -> (b -> d) -> p a b -> p c d     <<-------------- Or just this one
+  dimap :: (c -> a) -> (b -> d) -> p a b -> p c d                                             <<-------------- Or just this one
 
 
--- CoCartesian
-class Profunctor p  =>  Cocartesian p where
-  left'      ::  p  a  b   ->  p  (Either  a  c)  (Either  b  c)
-  right'     ::  p  a  b   ->  p  (Either  c  a)  (Either  c  b)
 
---Cartesian 
 class Profunctor p => Strong p where
   first'     ::  p  a  b   -> p  (a,  c)  (b,  c)
   second'    ::  p  a  b   -> p  (c,  a)  (c,  b)
 
 
+class Profunctor p  =>  Cocartesian p where
+  left'      ::  p  a  b   ->  p  (Either  a  c)  (Either  b  c)
+  right'     ::  p  a  b   ->  p  (Either  c  a)  (Either  c  b)
+
+
 class (Choice p, Strong p) => Traversing p where
-  traverse'  :: Traversable f => p a b -> p (f a) (f b)
-  wander     :: (forall f. Applicative f => (a -> f b) -> s -> f t) -> p a b -> p s t
+  traverse'  :: Traversable f => p a b -> p (f a) (f b)                                         <<-- Implement Either this ....
+  wander     :: (forall f. Applicative f => (a -> f b) -> s -> f t) -> p a b -> p s t           <<-- Or Implement this
+
+
+class (Functor t, Foldable t) => Traversable t where
+  traverse   :: Applicative f => (a -> f b) -> t a -> f (t b)
+  sequenceA  :: Applicative f => t (f a) -> f (t a)
 
 
 where p is a Profunctor : 
