78016a16ae55724f92ccaeadcb5d8b8bf8af8831
Michael Nyika <michael.nyika@unity3d.com>
2023-06-28 10:41:57 +0200

    fine

--- src/MultistarStrongSieveOpticsFun.hs
+++ src/MultistarStrongSieveOpticsFun.hs
@@ -18,17 +18,6 @@ To understand this better, read from top to bottom, in the style
 that the code was slowly built up
 
 
-class Profunctor p where
-        lmap  :: (c -> a) -> p a b -> p c b                 <<-----
-                                                                  |-------- Either implement these two ...
-        rmap  :: (b -> d) -> p a b -> p a d                 <<-----  
-
-        dimap :: (c -> a) -> (b -> d) -> p a b -> p c d     <<-------------- Or just this one
-
-
-
-where p is a Profunctor : 
-type Optic  p a b s t    =    p a b     ->    p s t
 
 
 
@@ -38,6 +27,10 @@ class Profunctor p where
   rmap  :: (b -> d) -> p a b -> p a d                 <<-----  
 
   dimap :: (c -> a) -> (b -> d) -> p a b -> p c d     <<-------------- Or just this one
+
+
+where p is a Profunctor : 
+type Optic  p a b s t    =    p a b     ->    p s t
 
 
 
