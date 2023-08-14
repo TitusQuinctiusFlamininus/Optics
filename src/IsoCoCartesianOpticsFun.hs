58ee96a0e6eccb9cb59fea21efd310a15e11dea5
Michael Nyika <michael.nyika@unity3d.com>
2023-08-14 15:11:59 +0200

    formatting

--- src/IsoCoCartesianOpticsFun.hs
+++ src/IsoCoCartesianOpticsFun.hs
@@ -40,17 +40,28 @@ instance  Profunctor (Iso a  b  )   where
 
 
 -- Alright, now we (attempt) to make it Choice, or CoCartesian 
--- We are able to make the case in this manner: 
--- ----->>> Explanation :  For Left'  :: The hin function is irrelevant, because no type variables appear for us to manipulate (s and t types)
+-- We are able to make the co-cartesian case successfully in the following manner: 
+-- --->>>>>>>>>>>>>>>>>> EXPLANTION
+-- ---->>>              :  For Left'  :: The hin function remains untouched, because no type variables appear for us to manipulate (s and t types)
 --                                    :: The her function: 
 --                                      -->> The transformation left' demands is from : (s  -> t)  into      :  ((Either s c)  -> (Either t c))
 --                                           Our input is now either a Left or Right structure, specifically :  (Left s)  or (Right c)       
---                                           (Left s) : then  we have a function that has s as input, but it must output (Either t c)  : (\s ->  Either t c)
+--                                           (Left s) : we have a function that has s as input, but it must output (Either t c)  : (\s ->  Either t c)
 --                                                    : Since the output must be "left-leaning" for s (for left'), the output must be restricted to   :  (Left t)
 --                                                    : So what we really have is a function:  (\s -> Left t) , or :  (\s -> Left . k $ s), or : (Left . k) 
 --                                           (Right c): then we have a function that has c as input, but it must output (Either t c)   : (\c ->  Either t c)
 --                                                    : Since the output must be "right-leaning)" for c (for left'), the output must be restricted to : (Right c)     
 --                                                    : So what we really have is a function:  (\c -> Right c), or :  Right    
+-- ----->>>             : For Right'  :: The hin function still remains untouched, because no type variables appear for us to manipulate (s and t types)
+--                                    :: The her function: 
+--                                      -->> The transformation right' demands is from : (s  -> t)  into      :  ((Either c s)  -> (Either c t))
+--                                           Our input is now either a Left or Right structure, specifically :  (Left c)  or (Right s)       
+--                                           (Left c) : we have a function that has c as input, but it must output (Either c t)  : (\c ->  Either c t)
+--                                                    : Since the output must be "left-leaning" for c (for right'), the output must be restricted to   :  (Left c)
+--                                                    : So what we really have is a function:  (\c -> Left c) , or :  Left
+--                                           (Right s): we have a function that has s as input, but it must output (Either c t)   : (\s ->  Either c t)
+--                                                    : Since the output must be "right-leaning)" for s (for right'), the output must be restricted to : (Right t)     
+--                                                    : So what we really have is a function:  (\s -> Right t), or : (\s -> Right (k s)) , or : (Right . k)  
 instance Choice (Iso a b) where
-  left'     (Iso f k)                     =   Iso  f   (either (Left . k) Right      )
-  right'    (Iso f k)                     =   Iso  f   (either  Left     (Right . k) )
+  left'     (Iso   f   k)            =   Iso   f   (either (Left . k) Right      )
+  right'    (Iso   f   k)            =   Iso   f   (either  Left     (Right . k) )
