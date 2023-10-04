972a68cc44e69afeddb20b79bda90afa97532bab
Michael Nyika <michael.nyika@unity3d.com>
2023-10-04 15:31:53 +0200

    introducing a new type

---
+++ src/BazaarOpticsFun.hs
@@ -0,0 +1,31 @@
+module BazaarOpticsFun where
+
+
+import Control.Lens.Combinators (Profunctor, dimap)
+
+{--
+
+To understand this better, read from top to bottom, in the style 
+that the code was slowly built up
+
+
+class Profunctor p where
+  lmap  :: (c -> a) -> p a b -> p c b                 <<-----
+                                                             |-------- Either implement these two ...
+  rmap  :: (b -> d) -> p a b -> p a d                 <<-----  
+
+  dimap :: (c -> a) -> (b -> d) -> p a b -> p c d     <<-------------- Or just this one
+
+
+where p is a Profunctor : 
+type    Optic  p  a  b  s  t =  p  a  b  ->  p  s  t
+
+
+newtype Bazaar p  a  b  t    =  Bazaar { runBazaar :: forall f. Applicative f => p a (f b) -> f t }
+
+
+
+--}
+
+---------------------------------------------------------------------------------
+
