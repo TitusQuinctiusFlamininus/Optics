f5c58956ae08c422d129f4a6a65ee0ce8471d46f
Ngumbah Michael Nyika <mnyika@gmail.com>
2026-08-26 12:04:10 +0200

    ok

--- src/TFunctor/Lens_TFunctorOpticsFun.hs
+++ src/TFunctor/Lens_TFunctorOpticsFun.hs
@@ -104,6 +104,6 @@ instance TFunctor (TFunctorLens s t) where
 -- TFunctor Optics definition (where p is a TFunctor)
 type TFunctorOptic p a b s t = p  a  b   ->  p  s  t
 
--- Creating a Generalized TFunctor Optic, but we need to supply the lens functions
+-- Creating a Generalized TFunctor Optic, but we need to supply the lens
 lensTFuncOptic :: (s -> a) -> ((b, s) -> t) -> TFunctorLens  a b a b  ->  TFunctorLens a b s t
-lensTFuncOptic x y (TLens g h)  = TLens x y
+lensTFuncOptic x y (TLens _ _)  = TLens x y
