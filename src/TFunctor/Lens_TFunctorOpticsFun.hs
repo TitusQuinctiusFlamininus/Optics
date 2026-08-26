90465e26710f58a0461b1496020721a0fa2d0f8c
Ngumbah Michael Nyika <mnyika@gmail.com>
2026-08-26 10:51:33 +0200

    Rename and some other stuff
    - we dont really need to express profunctor ideas here, since we are using a new definition of a transformative process, what we call the TFunctor

--- src/TFunctor/Lens_TFunctorOpticsFun.hs
+++ src/TFunctor/Lens_TFunctorOpticsFun.hs
@@ -1,22 +1,11 @@
-module TFunctor.LensOpticsFun where
-
-
-import Control.Lens.Combinators (Profunctor, dimap)
+module TFunctor.Lens_TFunctorOpticsFun where
 
 {-
 
 To understand this better, read from top to bottom, in the style 
 that the code was slowly built up
 
---First we define a regular Profunctor 
-class Profunctor p where
-  lmap  :: (c -> a) -> p a b -> p c b                 <<-----
-                                                             |-------- Either implement these two ...
-  rmap  :: (b -> d) -> p a b -> p a d                 <<-----  
-
-  dimap :: (c -> a) -> (b -> d) -> p a b -> p c d     <<-------------- Or just this one
-
-And a TFunctor can be visualized as: 
+A TFunctor can be visualized as: 
 
 
                          tmap
@@ -83,11 +72,16 @@ And a TFunctor can be visualized as:
                     ▼
                   p a' e
 
+So defining:
+class TFunctor p where
+   tmap :: (a' -> b) -> (k -> c -> e) -> (c -> k) -> p b c -> p a' e
+
+
 -}
 
 ---------------------------------------------------------------------------------
 
--- Defining a Tfunctor (where p is a Profunctor)
+-- Formally Defining a Tfunctor
 class TFunctor p where
   tmap :: (a' -> b) -> (k -> c -> e) -> (c -> k) -> p b c -> p a' e
 
@@ -97,24 +91,12 @@ data TFunctorLens a b s t         = TLens {  r  :: s         ->     a,
                                              r' :: (b, s)    ->     t 
                                            }
 
--- Turning our custom type into a Profunctor
-instance Profunctor (TFunctorLens s t) where
-    dimap h g (TLens l e)   =   TLens (l . h) $ \x  -> g $ e(fst x, h . snd $ x)
-
-
---For Convenience: remember how we defined a Tfunctor 
--- class TFunctor p where
---   tmap :: (a' -> b) -> (k -> c -> e) -> (c -> k) -> p b c -> p a' e
-
-
--- Now lets make it into a TFunctor 
 -- We end up with something close to a profunctor instance
 instance TFunctor (TFunctorLens s t) where
     tmap k m n (TLens l e) = TLens (l . k) $ \x -> let y = e (fst x, k (snd x)) in m (n y) y
 
----------------------------------------------------------------------------------
--- Since Optics definition:
-type Optic p a b s t = p a b -> p s t
 
--- Then TFunctor Optics defintion (where p' is a TFunctor)
-type TOptic p' a b s t = p' a b -> p' s t 
+---------------------------------------------------------------------------------
+
+-- TFunctor Optics definition (where p is a TFunctor)
+type TFunctorOptic p a b s t = p  a  b   ->  p  s  t
